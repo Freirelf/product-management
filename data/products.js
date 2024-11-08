4e4b8b036b7739638658bbf684343e227de28664
Lucas Freire <lucasfreirel.dev@gmail.com>
2024-11-08 17:10:27 -0300

    implement first config EX

---
+++ data/products.js
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    id: 1,
+    name: "Notebook",
+    category: "Eletrônicos",
+    price: 1000
+  },
+  {
+    id: 2,
+    name: "Tablet",
+    category: "Eletrônicos",
+    price: 800
+  },
+  {
+    id: 3,
+    name: "Camiseta",
+    category: "Vestuário",
+    price: 300
+  }
+]
